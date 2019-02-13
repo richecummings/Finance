c010c7f3971bb59f9a3d98195bcddc7c5e125286
Richard Cummings <richardecummings@gmail.com>
2019-02-12 22:36:02 -0500

    Initial commit

---
+++ Finance/Interfaces/IShare.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Finance.Interfaces
+{
+    public interface IShare
+    {
+        Task Show(string title, string url);
+    }
+}
---
+++ Finance/View/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Finance
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+    }
+}
